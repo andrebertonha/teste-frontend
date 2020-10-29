f5378e83659d47bd3d0896e2f8facd49409f5d2d
andre <andrebertonhadev@gmail.com>
2020-10-29 08:31:00 -0700

    connection to database sucessfull

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
 const express = require('express');
-const app = express();
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+
+const app = express();
+
+/**
+    Database setup
+ */
+
+ mongoose.connect('mongodb://localhost:27017/uploadFiles', {
+     useNewUrlParser: true,
+     useUnifiedTopology: true,
+ });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
