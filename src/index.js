e2db797f4a8e756bebe81bc5ee357d1e6ef885ca
andre <andrebertonhadev@gmail.com>
2020-10-29 06:00:58 -0700

    routes and morgan

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 const express = require('express');
-
 const app = express();
+const morgan = require('morgan');
 
-app.get('/', (req, res) => {
-    return res.send('hello express');
-});
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
+
+app.use(require('./routes'));
 
 app.listen(3000);
